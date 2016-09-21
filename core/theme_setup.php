fe2b21a104113e1c4e35008e8cddc50e2d99d076
codernize <codernize@gmail.com>
2016-09-21 11:33:40 +0300

    add typekit enqueue

--- core/theme_setup.php
+++ core/theme_setup.php
@@ -58,6 +58,21 @@ add_action('wp_enqueue_scripts', 'add_jquery_script');
 
 
 /**
+ * TypeKit Fonts
+ */
+function theme_typekit() {
+    wp_enqueue_script( 'theme_typekit', '//use.typekit.net/xxxxxxx.js');
+}
+// add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+
+function theme_typekit_inline() {
+  if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+}
+// add_action( 'wp_head', 'theme_typekit_inline' );
+
+/**
  * This theme uses wp_nav_menus() for the header menu, utility menu and footer menu.
  */
 register_nav_menus( array(
