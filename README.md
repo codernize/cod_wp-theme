4a278f1e2aae2129e19e7738d20d6ec0d2202eac
codernize <codernize@gmail.com>
2019-06-14 19:33:06 +0300

    update to gulp v4, package.json install foundation-6 && updated readme.md

--- README.md
+++ README.md
@@ -3,15 +3,15 @@ based on things that I discover to be usefull.
 
 ## How to
 
-Has Foundation 6 for sites, basic template. To install just run
 
-
-```
-bower install
-```
-then 
+Run
 
 ```
 npm install
 ```
 
+then 
+
+```
+gulp default
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,70 @@
-var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+// veni vidi codi
+// gulpfile with gulp 4 keeping old system working
 
-var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
-];
+"use strict";
 
-gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
-});
+var gulp  = require('gulp'),
+    sass  = require('gulp-sass'),
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify'),
+    rename  = require('gulp-rename'),
+    autoprefixer  = require('autoprefixer');
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
-});
+var paths = {
+        styles: {
+            src: './scss/*.scss',
+            dest: './css/'
+        },
+        scripts: {
+            src: './js/app.js',
+            dest: './js/dest/'
+        }
+};
+
+function styles() {
+    return gulp
+        .src(paths.styles.src, {
+            sourcemaps: true
+        })
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        /*.pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))*/
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+function scripts() {
+    return gulp
+        .src(paths.scripts.src, {
+            /*sourcemaps: true*/
+        })
+        .pipe(uglify())
+        // .pipe(concat('app.min.js'))
+        .pipe(
+            rename({
+                suffix: '.min'
+            })
+        )
+        .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    // gulp.watch(paths.scripts.src, scripts); 
+}
+
+const defualt = gulp.parallel(styles, watch);
+
+
+
+// gulp.task(defualt);
+// gulp.task('default', defualt);
+// In the past, task() was used to register your functions as tasks. While that API is still available, exporting should be the primary registration mechanism, except in edge cases where exports won't work.
+
+// export tasks
+exports.watch = watch;
+exports.default = defualt;
+exports.styles = styles;
+exports.scripts = scripts;
