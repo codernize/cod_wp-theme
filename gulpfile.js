001a34f54f53da61ae7e382535c429b9695df41b
codernize <codernize@gmail.com>
2019-06-14 20:23:45 +0300

    update gulp & package.json to contain proper dependecies & tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,59 @@
-// veni vidi codi
-// gulpfile with gulp 4 keeping old system working
+// Include gulp
+var gulp = require('gulp');
 
-"use strict";
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
-var gulp  = require('gulp'),
-    sass  = require('gulp-sass'),
-    concat  = require('gulp-concat'),
-    uglify  = require('gulp-uglify'),
-    rename  = require('gulp-rename'),
-    autoprefixer  = require('autoprefixer');
+var path = require("path");
 
-var paths = {
-        styles: {
-            src: './scss/*.scss',
-            dest: './css/'
-        },
-        scripts: {
-            src: './js/app.js',
-            dest: './js/dest/'
-        }
-};
+const SASS_INCLUDE_PATHS = [
+    path.join(__dirname, '/node_modules/foundation-sites/_vendor/normalize-scss/sass'),
+    path.join(__dirname, '/node_modules/foundation-sites/scss')
 
-function styles() {
-    return gulp
-        .src(paths.styles.src, {
-            sourcemaps: true
-        })
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        /*.pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie >= 9']
-        }))*/
-        .pipe(gulp.dest(paths.styles.dest));
-}
+];
 
-function scripts() {
-    return gulp
-        .src(paths.scripts.src, {
-            /*sourcemaps: true*/
-        })
-        .pipe(uglify())
-        // .pipe(concat('app.min.js'))
-        .pipe(
-            rename({
-                suffix: '.min'
-            })
-        )
-        .pipe(gulp.dest(paths.scripts.dest));
-}
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
-function watch() {
-    gulp.watch(paths.styles.src, styles);
-    // gulp.watch(paths.scripts.src, scripts); 
-}
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        // .pipe(sourcemaps.init())
+        .pipe(sass(
+            {
+                outputStyle: 'compressed',
+                includePaths: SASS_INCLUDE_PATHS
+            }
+            ))
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+});
 
-const defualt = gulp.parallel(styles, watch);
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('js/dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify({ mangle: false }))
+        .on('error', function (err) { console.log( err ) })
+        .pipe(gulp.dest('js/dist'));
+});
 
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', [/*'lint',*/ 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
 
-
-// gulp.task(defualt);
-// gulp.task('default', defualt);
-// In the past, task() was used to register your functions as tasks. While that API is still available, exporting should be the primary registration mechanism, except in edge cases where exports won't work.
-
-// export tasks
-exports.watch = watch;
-exports.default = defualt;
-exports.styles = styles;
-exports.scripts = scripts;
+// Default Task
+gulp.task('default', [ 'sass', 'scripts', 'watch']);
